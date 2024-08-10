6e6b28d2d153efdafbf007dd4e57573faf15484a
Sihuai2412 <139676314+Sihuai2412@users.noreply.github.com>
2024-08-10 21:41:06 +0800

    初步更改配方

---
+++ kubejs/startup_scripts/register.js
@@ -0,0 +1,10 @@
+StartupEvents.registry("block", (event) => {
+    event.create("military_casing")
+    .displayName("军用外壳")
+    .material("stone")
+    .hardness(5.0)
+    .resistance(2.0)
+    .requiresTool(true)
+    .tagBlock("mineable/pickaxe")
+    .tagBlock('minecraft:needs_stone_tool')
+})
