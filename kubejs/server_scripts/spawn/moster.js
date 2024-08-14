dece14bc97cfa4d35b2cf05306470de5d94259a5
KL_jiana <2436262516@qq.com>
2024-08-14 13:38:17 +0800

    update

--- kubejs/server_scripts/spawn/moster.js
+++ kubejs/server_scripts/spawn/moster.js
@@ -1,5 +1,5 @@
 EntityEvents.spawned(event => {
     const { entity } = event
     //console.info(`${entity.toString()}`)
-    entity.monster || entity.type == "minecraft:bat" ? event.cancel() : event.success;
+    //entity.monster || entity.type == "minecraft:bat" ? event.cancel() : event.success;
 })
