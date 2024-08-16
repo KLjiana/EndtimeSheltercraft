70d68802dac46218a8fe1a2ce795ff24191ebb01
KL_jiana <2436262516@qq.com>
2024-08-16 23:08:19 +0800

    怪物列表

--- kubejs/server_scripts/spawn/monster.js
+++ kubejs/server_scripts/spawn/monster.js
@@ -1,5 +1,64 @@
+let type_1 = ["zombie_island:runner",
+    "zombie_island:walker",
+    "zombie_island:caustic_x_walker",
+    "zombie_island:insect_swarm_walker",
+    "zombie_island:riot_gear_walker",
+    "zombie_island:grenadier_walker",
+    "zombie_island:shambler",
+    "zombie_island:firefighter_walker",
+    "zombie_island:hazmat_walker",
+    "zombie_island:burning_walker",
+    "zombie_island:shocking_walker",
+    "zombie_island:incendiary_walker",
+    "zombie_island:eat_courier"]
+
+let type_2 = ["rotted:marine",
+    "rotted:shielder",
+    "rotted:flusk",
+    "rotted:adventurer",
+    "rotted:miner",
+    "rotted:farmer",
+    "rotted:doctor",
+    "minecraft:vindicator",
+    "minecraft:evoker",
+    "minecraft:witch",
+    "minecraft:wither_skeleton",
+    "minecraft:blaze",
+    "minecraft:phantom"]
+
+let type_3 = ["minecraft:cave_spider",
+    "eeeabsmobs:corpse_warlock_egg",
+    "tacz_npcs:terrorist_no_armor",
+    "tacz_npcs:terrorist_scav_pistol_armored",
+    "tacz_npcs:terrorist_scav",
+    "mutantmonsters:spider_pig",
+    "mutantmonsters:mutant_zombie",
+    "minecraft:ghast",
+    "minecraft:slime",
+    "minecraft:spider",
+    "minecraft:ravager",
+    "minecraft:silverfish",
+    "minecraft:skeleton",
+    "minecraft:zoglin"]
+
+let type_4 = ["themutantmobsmod:mutantstray",
+    "aqua_creepers:aqua_creeper",
+    "minecraft:creeper",
+    "mutantmonsters:mutant_creeper",
+    "themutantmobsmod:mutant_drowned",
+    "tntslimes:tnt_slime",
+    "themutantmobsmod:mutant_zombified_piglin",
+    "minecraft:warden",
+    "eeeabsmobs:nameless_guardian_egg",
+    "mutantmonsters:mutant_skeleton",
+    "mutantmonsters:mutant_enderman",
+    "themutantmobsmod:gigaillager",
+    "minecraft:zombie"]
+
+let type_5 = ["minecraft:zombified_piglin"]
+
 EntityEvents.spawned(event => {
     const { entity } = event
     //console.info(`${entity.toString()}`)
-    //entity.monster || entity.type == "minecraft:bat" ? event.cancel() : event.success;
+    entity.type.indexOf("zombie_island:runner") != -1 ? event.cancel() : event.success;
 })
