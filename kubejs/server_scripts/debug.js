c3fe5c520a446031e02b55656e1ce3910cd19b8e
KL_jiana <2436262516@qq.com>
2024-08-29 00:28:13 +0800

    1

--- kubejs/server_scripts/debug.js
+++ kubejs/server_scripts/debug.js
@@ -3,9 +3,9 @@ let dumpList = []
 PlayerEvents.chestClosed(event => {
     dumpList = []
     event.inventory.allItems.forEach(item => {
-        item = item.id.replace("_spawn_egg", "")
+        //item = item.id.replace("_spawn_egg", "")
 
-        dumpList.push(`event.add('esc:type_1','${item}')`)
+        dumpList.push(`"${item}"`)
     })
     let copyText = Component.literal("已导出箱子中的实体id：点击复制").green().hover("快来复制我ヾ(≧▽≦*)o").clickCopy(dumpList)
     event.server.tell(copyText)
